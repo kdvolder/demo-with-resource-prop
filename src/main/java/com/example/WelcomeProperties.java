f56798d83849712f9f0078ee5771f32aa7209923
Kris De Volder <kdevolder@pivotal.io>
2016-05-10 09:34:03 -0700

    Add example for list-value reconcile

--- src/main/java/com/example/WelcomeProperties.java
+++ src/main/java/com/example/WelcomeProperties.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 public class WelcomeProperties {
 
 	private Resource resource;
+	private List<Color> colors;
 
 	public Resource getResource() {
 		return resource;
@@ -17,5 +20,15 @@ public class WelcomeProperties {
 	public void setResource(Resource resource) {
 		this.resource = resource;
 	}
+
+	public List<Color> getColors() {
+		return colors;
+	}
+
+	public void setColors(List<Color> colors) {
+		this.colors = colors;
+	}
+	
+	
 	
 }
